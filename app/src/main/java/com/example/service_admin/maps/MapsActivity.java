0dabbdf028cae8608da3bc23e501a3445e12ba5d
jasonsilva15 <Kathe2006>
2019-03-02 11:54:56 -0600

    Maps

--- app/src/main/java/com/example/service_admin/maps/MapsActivity.java
+++ app/src/main/java/com/example/service_admin/maps/MapsActivity.java
@@ -149,7 +149,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         };
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
             // TODO: Consider calling
+
+
+
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
